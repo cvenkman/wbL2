3bf78d3b44dc6afd052383d2347625e39cf4afda
Carlos Venkman <cvenkman@ge-g5.kzn.21-school.ru>
2022-06-09 22:49:45 +0300

    try test dev02

---
+++ develop/dev02/task.go
@@ -0,0 +1,84 @@
+package main
+
+/*
+=== Задача на распаковку ===
+
+Создать Go функцию, осуществляющую примитивную распаковку строки, содержащую повторяющиеся символы / руны, например:
+	- "a4bc2d5e" => "aaaabccddddde"
+	- "abcd" => "abcd"
+	- "45" => "" (некорректная строка)
+	- "" => ""
+Дополнительное задание: поддержка escape - последовательностей
+	- qwe\4\5 => qwe45 (*)
+	- qwe\45 => qwe44444 (*)
+	- qwe\\5 => qwe\\\\\ (*)
+
+В случае если была передана некорректная строка функция должна возвращать ошибку. Написать unit-тесты.
+
+Функция должна проходить все тесты. Код должен проходить проверки go vet и golint.
+*/
+
+import (
+	"errors"
+	// "fmt"
+	"strconv"
+	"unicode"
+)
+
+func main() {
+	// fmt.Println(Convert("a4bs2"))
+	// fmt.Println(Convert("a10b"))
+	// fmt.Println(Convert("abab"))
+	// fmt.Println(Convert("45"))
+	// fmt.Println(Convert(""))
+}
+
+func Convert(before string) (string, error) {
+	if len(before) == 0 {
+		return before, nil
+	}
+
+	res := make([]rune, 0, len(before))
+
+	_, err := strconv.Atoi(string(before[0]))
+	if err == nil {
+		return "", errors.New("must be at least one char")
+	}
+
+	i := 0
+
+	for ; i < len(before) - 1; i++ {
+		// if next char is not a digit
+		if !unicode.IsDigit(rune(before[i + 1])) {
+			res = append(res, rune(before[i]))
+			continue
+		}
+		
+		num, numCount := parseNumber(before[i+1:])
+		for ; num > 0; num-- {
+			res = append(res, rune(before[i]))
+		}
+		i += numCount
+	}
+
+	// add last char if it's not a digit
+	if i < len(before) {
+		res = append(res, rune(before[i]))
+	}
+	
+	return string(res), nil
+}
+
+// возвращает число и количсевто цифр в нем
+func parseNumber(str string) (int, int) {
+	num := 0
+
+	for i, ch := range str {
+		tmpNum, err := strconv.Atoi(string(ch))
+		if err != nil {
+			return num, i
+		}
+		num = num * 10 + tmpNum
+	}
+	return num, len(str) + 1
+}
---
+++ develop/dev02/task_test.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"testing"
+	"github.com/stretchr/testify"
+)
+
+func TestConvert(t *testing.T) {
+	testCases := []struct{
+		{
+			name string
+			in string
+			out string
+		}
+	} {
+		{
+			name: "a4bs2"
+			in: "a4bs2"
+			out: "aaaabss"
+		}
+		{
+			name: "all chars"
+			in: "abab"
+			out: "abab"
+		}
+		{
+			name: "a10b"
+			in: "a10b"
+			out: "a10b"
+		}
+		{
+			name: "empty"
+			in: ""
+			out: ""
+		}
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			assert.Equal(t, testCase.out, Convert(testCase.in))
+		})
+	}
+}
