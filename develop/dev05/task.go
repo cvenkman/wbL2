83b788a79434ad78750b4f2c9c0c3621b1f19c8b
Carlos Venkman <cvenkman@ge-g5.kzn.21-school.ru>
2022-06-14 01:15:39 +0300

    nade 05 with -b

--- develop/dev05/task.go
+++ develop/dev05/task.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
 /*
 === Утилита grep ===
 
@@ -18,6 +26,80 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+type flags struct {
+	b bool
+}
 
+// type results struct
+
+type result struct {
+	before int
+	found string
+}
+
+func main() {
+	flags := flags{}
+	flag.BoolVar(&flags.b, "b", false, "reverse sort")
+	flag.Parse()
+	args := flag.Args()
+	
+	toSerach := args[0]
+	
+	args = args[1:]
+	
+	// fmt.Println(args, toSerach)
+
+	for _, arg := range args {
+		file, err := os.Open(arg)
+		if err != nil {
+			fmt.Fprint(os.Stderr, "grep: ", err)
+			continue
+		}
+		data, err := getFileData(file)
+		if err != nil {
+			fmt.Fprint(os.Stderr, "grep: ", err)
+		}
+		
+		// получаем массив структур с найденными словами из файла
+		res := search(data, toSerach, flags)
+		// fmt.Println(res)
+		for _, el := range res {
+			if flags.b {
+				fmt.Printf("%s:%d: %s\n", file.Name(), el.before, el.found)
+			} else {
+				fmt.Printf("%s: %s\n", file.Name(), el.found)
+			}
+		}
+	}
+
+}
+
+func search(data []string, toSerach string, flags flags) []result {
+	res := make([]result, 0)
+	i := 0
+
+	// идем по массиву строк из файла
+	for _, str := range data {
+		if strings.Contains(str, toSerach) {
+			res1 := result{before: i, found: str}
+			res = append(res, res1)
+		}
+		// видимо включая \0
+		i += len(str) + 1
+	}
+	return res
+}
+
+func getFileData(file *os.File) ([]string, error) {
+
+	// if several join to one slice
+	defer file.Close()
+	// read file
+	data, err := ioutil.ReadFile(file.Name())
+	if err != nil {
+		return nil, err
+	}
+
+	// split file
+	return strings.Split(string(data), "\n"), nil
 }
--- develop/dev05/task_test.go
+++ develop/dev05/task_test.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConvert(t *testing.T) {
+	testCases := []struct {
+		name	string
+		flags	flags
+		in		[]string
+		out		[]result
+	}{
+		// {
+		// 	name:  "grep one file",
+		// 	flags: flags{},
+		// 	in:    []string{"aa", "cat", "\n","d", "b"},
+		// 	out:   []string{"\n", "a", "b", "c", "d"},
+		// 	err:   nil,
+		// },
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			// res, err := sortFile(testCase.flags, testCase.in)
+			assert.Equal(t, testCase.out, res)
+			assert.Equal(t, testCase.err, err)
+		})
+	}
+}
