6094e63f08f2e01fbe4c57872064dddf0e56c672
Carlos Venkman <cvenkman@ge-g5.kzn.21-school.ru>
2022-06-15 14:42:38 +0300

    done -n flag

--- develop/dev03/task.go
+++ develop/dev03/task.go
@@ -8,7 +8,7 @@ import (
 	"log"
 	"os"
 	"reflect"
-	// "regexp"
+	"regexp"
 	"sort"
 	"strconv"
 	"strings"
@@ -140,62 +140,23 @@ func sortWithFlags(flags flags, dataToSort []string) []string {
 	}
 
 	if flags.n {
-		// r := regexp.MustCompile(`[^0-9]+|[0-9]+`)
-		// fmt.Println("-", r.)
 
+		re := regexp.MustCompile("[+-]?([0-9]*[.])?[0-9]+")
 		sort.Slice(dataToSort, func(i, j int) bool {
-			if dataToSort[i] == "" || dataToSort[j] == "" {
+			firstStr := re.FindString(dataToSort[i])
+			secondStr := re.FindString(dataToSort[j])
+			// если числа нет
+			if firstStr == "" && secondStr == "" {
+				// если обе строки - ставим их по возрастанию
+				return dataToSort[i] < dataToSort[j]
+			}
+			if firstStr == "" || secondStr == "" {
+				// если только одна строка - оставляем как есть
 				return true
 			}
-
-			dataI := ""
-			dataJ := ""
-
-			if isNumber(dataToSort[i][0]) {
-				tmpI := make([]byte, 1)
-				for q := 0; q < len(dataToSort[i]) && isNumber(dataToSort[i][q]); q++ {
-					tmpI = append(tmpI, dataToSort[i][q])
-				}
-				dataI = string(tmpI)
-			// fmt.Println("-", dataToSort[i], dataI)
-
-			}
-			if isNumber(dataToSort[j][0]) {
-				tmpJ := make([]byte, 1)
-				for q := 0;  q < len(dataToSort[j]) && isNumber(dataToSort[j][q]); q++ {
-					tmpJ = append(tmpJ, dataToSort[j][q])
-				}
-				dataJ = string(tmpJ)
-			// fmt.Println("+", dataToSort[j], dataJ)
-
-			}
-
-			if dataI == "" {
-				dataI = dataToSort[i]
-			}
-			if dataJ == "" {
-				dataJ = dataToSort[j]
-			}
-
-			
-			fmt.Println("-", dataI, dataJ)
-			val1, err1 := strconv.ParseFloat(dataI, 64) // переводим первую строку в число
-			val2, err2 := strconv.ParseFloat(dataJ, 64) // переводим вторую строку в число
-			if err1 != nil && err2 != nil {
-				return dataI[0] < dataJ[0]
-			}
-			fmt.Println("+")
-			
-			if err1 != nil {
-				// fmt.Println("Error: ", error)
-				return true
-			}
-			if err2 != nil {
-				// fmt.Println("Error: ", err)
-				return true
-			}
-			// fmt.Println(val1, val2)
-			return val1 < val2
+			first, _ := strconv.ParseFloat(firstStr, 64)
+			second, _ := strconv.ParseFloat(secondStr, 64)
+			return first < second
 		})
 	}
 
